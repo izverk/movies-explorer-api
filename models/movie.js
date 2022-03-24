48858f65171c68b28da6042de51349feddae90bc
Ilya Zverkov <izverk78@yandex.ru>
2022-03-24 17:24:19 +0300

    добавляет схемы и модели данных для сущностей user и movie

---
+++ models/movie.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const { incorrectCardLink } = require('../utils/constants');
+
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    match: [
+      /^https?:\/\/([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z0-9]+)(\/[\w-.~:?#[\]@!$&'()*+,;=]+)*#?\/?$/,
+      incorrectCardLink,
+    ],
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    match: [
+      /^https?:\/\/([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z0-9]+)(\/[\w-.~:?#[\]@!$&'()*+,;=]+)*#?\/?$/,
+      incorrectCardLink,
+    ],
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    match: [
+      /^https?:\/\/([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z0-9]+)(\/[\w-.~:?#[\]@!$&'()*+,;=]+)*#?\/?$/,
+      incorrectCardLink,
+    ],
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('card', movieSchema);
